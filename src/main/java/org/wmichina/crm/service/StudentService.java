b9a08e8e1dba3aa080556cb66a5f281dd61d8c35
AndyZhou <navyzh1979@gmail.com>
2012-12-11 18:10:26 +0800

    add StudentService

---
+++ src/main/java/org/wmichina/crm/service/StudentService.java
@@ -0,0 +1,11 @@
+package org.wmichina.crm.service;
+
+import java.util.List;
+
+import org.wmichina.crm.entity.BizActivity;
+import org.wmichina.crm.entity.BizChannel;
+import org.wmichina.crm.helper.ResultPageBuilder;
+
+public interface StudentService {
+
+}
